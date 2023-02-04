7b704adb60e34be52a16f9ea28f82313484c5a3c
ivanzgelaRPPP <ivanzgelagit@gmail.com>
2023-02-04 21:17:58 +0100

    Set up startup.cs

--- Buildings/Buildings/Startup.cs
+++ Buildings/Buildings/Startup.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace Buildings
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-
-
 
         }
     }
